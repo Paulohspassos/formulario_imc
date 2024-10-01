8183efc880ccc8145e3be9f6b9e43d7a975fdb3d
Paulohspassos <paulohspassos@gmail.com>
2024-10-01 17:58:05 -0300

    iniciado inserção de javascript

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector('#idSubmit');
+
+
+
+btn.addEventListener('click', e => {
+    e.preventDefault()
+    const nome = document.querySelector('#idNome');
+    const dnasc = document.querySelector('#idNasc');
+    const peso = documento.querySelector('#idPeso');
+    const altura = document.querySelector('#idAltura');
+    const result = document.querySelector('#res');
+
+    
+})
